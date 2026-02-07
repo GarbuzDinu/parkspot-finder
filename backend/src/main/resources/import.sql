3766d0e8dfe8503666a2a63dd833361bd6510c8c
Dinu Garbuz <dinu.garbuz@gmail.com>
2026-02-07 19:49:15 +0200

    add import.sql

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO park_slot (id, name, capacity, occupied, address, city, rating, price) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'Centrall Park', 100, 20, '123 Main St', 'New York', 4.5, 15.0);
